cff0cb78859883900e842ce9fed41a236323f55e
djavagit <58994972+djavagit@users.noreply.github.com>
2020-10-16 13:32:46 +0200

    ajout classe Activity_result.java + manifest

---
+++ app/src/main/java/com/example/myapplication/mistzerrandomizer/activity/Activity_result.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.mistzerrandomizer.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.mistzerrandomizer.R;
+
+public class Activity_result extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
