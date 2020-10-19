5f0758bcc5c6d97bb3703553290eb1b67d55a65d
djavagit <58994972+djavagit@users.noreply.github.com>
2020-10-19 18:33:10 +0200

    activity_result test avec yi

--- app/src/main/java/com/example/myapplication/mistzerrandomizer/activity/Activity_result.java
+++ app/src/main/java/com/example/myapplication/mistzerrandomizer/activity/Activity_result.java
@@ -1,8 +1,11 @@
 package com.example.myapplication.mistzerrandomizer.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +40,101 @@ public class Activity_result extends AppCompatActivity {
         ((TextView) findViewById(R.id.textView_sum8)).setText(summoner_list.get(7));
         ((TextView) findViewById(R.id.textView_sum9)).setText(summoner_list.get(8));
         ((TextView) findViewById(R.id.textView_sum10)).setText(summoner_list.get(9));
+
+        //final Context context = getApplicationContext();
+        final int duration = Toast.LENGTH_SHORT;
+
+        ImageView i1 = ((ImageView) findViewById(R.id.img_sum_1));
+        i1.setImageResource(R.drawable.masteryi);
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i2 = ((ImageView) findViewById(R.id.img_sum_2));
+        i2.setImageResource(R.drawable.masteryi);
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i3 = ((ImageView) findViewById(R.id.img_sum_3));
+        i3.setImageResource(R.drawable.masteryi);
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i4 = ((ImageView) findViewById(R.id.img_sum_4));
+        i4.setImageResource(R.drawable.masteryi);
+        i4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i5 = ((ImageView) findViewById(R.id.img_sum_5));
+        i5.setImageResource(R.drawable.masteryi);
+        i5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i6 = ((ImageView) findViewById(R.id.img_sum_6));
+        i6.setImageResource(R.drawable.masteryi);
+        i6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i7 = ((ImageView) findViewById(R.id.img_sum_7));
+        i7.setImageResource(R.drawable.masteryi);
+        i7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i8 = ((ImageView) findViewById(R.id.img_sum_8));
+        i8.setImageResource(R.drawable.masteryi);
+        i8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i9 = ((ImageView) findViewById(R.id.img_sum_9));
+        i9.setImageResource(R.drawable.masteryi);
+        i9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+        ImageView i10 = ((ImageView) findViewById(R.id.img_sum_10));
+        i10.setImageResource(R.drawable.masteryi);
+        i10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Maitre yi", duration);
+                toast.show();
+            }
+        });
+
 
     }
 }
