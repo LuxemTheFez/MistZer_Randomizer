a0ad2b8dee27624467a075d7fc9c2c1a105d5d3f
Loris <loris.lacerenza@gmail.com>
2020-11-11 18:48:28 +0100

    Changement input nom fonctionnel mais améliorable

--- app/src/main/java/com/example/myapplication/mistzerrandomizer/activity/Activity_names.java
+++ app/src/main/java/com/example/myapplication/mistzerrandomizer/activity/Activity_names.java
@@ -1,16 +1,22 @@
 package com.example.myapplication.mistzerrandomizer.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.mistzerrandomizer.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_names extends AppCompatActivity {
 
@@ -20,62 +26,79 @@ public class Activity_names extends AppCompatActivity {
         return (!s.isEmpty() && s.length()<=16);
     }
 
+    public List<String> summoner_list;
+
+    ProgressBar simpleProgressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_names);
+
+        final EditText edit_1 = (EditText) findViewById(R.id.text_name1);
+        final TextView nom = (TextView) findViewById(R.id.name1);
+
+        final ArrayList<String> summoner_list = new ArrayList<String>();
+
+        simpleProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        simpleProgressBar.setProgress(0);
+
+        nom.setText("Invocateur " + (summoner_list.size()+1));
+        edit_1.setHint("Invocateur " + (summoner_list.size()+1));
+
+        final Button button_nom = findViewById(R.id.button_nom);
+        button_nom.setText("Suivant");
+        button_nom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("avant : " +summoner_list.size());
+                String sum = edit_1.getText().toString();
+                if (sum_Valid(sum)){
+                    summoner_list.add(sum);
+                    nom.setText("Invocateur " + (summoner_list.size()+1));
+                    edit_1.setHint("Invocateur " + (summoner_list.size()+1));
+
+                    System.out.println(summoner_list.size());
+                    edit_1.getText().clear();
+                    simpleProgressBar.incrementProgressBy(10);
+                    if (summoner_list.size()==10) {
+                        closeKeyboard();
+                        edit_1.setVisibility(View.GONE);
+                        button_nom.setVisibility(View.GONE);
+                        nom.setText("Cliquer sur continuer pour continuer");
+                    }
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Pseudo non valide", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+
 
         Button button_continue = findViewById(R.id.button_names);
         button_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                EditText edit_1 = (EditText) findViewById(R.id.text_name1);
-                EditText edit_2 = (EditText) findViewById(R.id.text_name2);
-                EditText edit_3 = (EditText) findViewById(R.id.text_name3);
-                EditText edit_4 = (EditText) findViewById(R.id.text_name4);
-                EditText edit_5 = (EditText) findViewById(R.id.text_name5);
-                EditText edit_6 = (EditText) findViewById(R.id.text_name6);
-                EditText edit_7 = (EditText) findViewById(R.id.text_name7);
-                EditText edit_8 = (EditText) findViewById(R.id.text_name8);
-                EditText edit_9 = (EditText) findViewById(R.id.text_name9);
-                EditText edit_10 = (EditText) findViewById(R.id.text_name10);
 
-                ArrayList<String> summoner_list = new ArrayList<String>();
 
-                String summoner_1 = edit_1.getText().toString();
-                String summoner_2 = edit_2.getText().toString();
-                String summoner_3 = edit_3.getText().toString();
-                String summoner_4 = edit_4.getText().toString();
-                String summoner_5 = edit_5.getText().toString();
-                String summoner_6 = edit_6.getText().toString();
-                String summoner_7 = edit_7.getText().toString();
-                String summoner_8 = edit_8.getText().toString();
-                String summoner_9 = edit_9.getText().toString();
-                String summoner_10 = edit_10.getText().toString();
 
-                summoner_list.add(summoner_1);
-                summoner_list.add(summoner_2);
-                summoner_list.add(summoner_3);
-                summoner_list.add(summoner_4);
-                summoner_list.add(summoner_5);
-                summoner_list.add(summoner_6);
-                summoner_list.add(summoner_7);
-                summoner_list.add(summoner_8);
-                summoner_list.add(summoner_9);
-                summoner_list.add(summoner_10);
-
-                if (sum_Valid(summoner_1) && sum_Valid(summoner_2) &&
-                        sum_Valid(summoner_3) && sum_Valid(summoner_4) &&
-                        sum_Valid(summoner_5) && sum_Valid(summoner_6) &&
-                        sum_Valid(summoner_7) && sum_Valid(summoner_8) &&
-                        sum_Valid(summoner_9) && sum_Valid(summoner_10)) {
                     Intent Activity_result_intent = new Intent(getApplicationContext(), Activity_result.class);
                     Activity_result_intent.putExtra(EXTRA_LIST_SUMMONER, summoner_list);
                     startActivity(Activity_result_intent);
                 }
-            }
+
         });
 
+    }
+
+    private void closeKeyboard(){
+        View view = this.getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
